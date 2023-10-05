bfc31b6b5ad79a3db718a763a70459a336ac25e2
amogh-iyer <amoghiyer2002@gmail.com>
2023-10-05 18:00:56 -0400

    v

--- cs361-main/cs361languages/src/mystack/MyStack.java
+++ cs361-main/cs361languages/src/mystack/MyStack.java
@@ -3,6 +3,8 @@
  */
 package mystack;
 
+import org.w3c.dom.Node;
+
 /**
  * @author ADD YOUR NAME 
  *
@@ -24,21 +26,46 @@ public class MyStack<T> {
 		}
 	}
 
+	
 	private MyNode<T> theStack = null;
-
+    private MyNode<T> top;
 	/**
 	 * 
 	 */
 	public MyStack() {
+        top = null;
 	}
 
 	public T pop() {
 		// TODO To complete
-		return null;
+		MyNode<T> temp = null;
+		if(top != null) {
+			if(top.next != null ) {
+				temp = top;
+				top = top.next;
+				
+			} else {
+				temp = top;
+				top = null;
+			}
+		}
+		return temp.val;
 	}
+	
+	
 
 	public void push(T v) {
 		// TODO To complete
+		MyNode<T> node = new MyNode<T>(v, top);
+	if(top == null) {
+		top = node;
+		
+	} else {
+		node.next = top;
+		top = node;
+		
+		
+	}
 	}
 
 	
@@ -48,12 +75,22 @@ public class MyStack<T> {
 	public static void main(String[] args) {
 		// TODO To complete
 		// Create a stack of Integer
+		MyStack<Integer> num = new MyStack<Integer>();
+		num.push(1);
+		num.push(2);
+		num.pop();
+		System.out.println("The number removed: " + num.pop());
+		num.push(5);
 		// Push 1 and 2
 		// Pop
 		// Push 5
 		
 		// TODO To complete
 		// Create a stack of Person
+		MyStack<String> person = new MyStack<String>();
+		person.push("Amogh");
+		person.push("Pride");
+		System.out.println("The person removed " + person.pop());
 		// Push a person p1 with your name
 		// Push a person p2 with my name
 	}
